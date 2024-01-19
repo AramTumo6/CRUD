dbad7041e16a4d6c84bb2580c9df6e9cba913895
AraratIskenian <ararat.iskenian.y@tumo.org>
2024-01-19 21:27:31 +0400

    ConnectMongodb

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require("express");
 var path = require("path");
 const bodyParser = require('body-parser');
@@ -23,4 +24,30 @@ app.post('/addName', (req, res) => {
 app.listen(3000, function(){
    console.log("Example is running on port 3000");
 });
+*/
+
+
+
+const mongoose = require('mongoose');
+
+
+// Replace the connection string with your MongoDB connection string
+const connectionString = 'mongodb+srv://aramharutyunyan6:tumo1234@cluster0.f1nip32.mongodb.net/?retryWrites=true&w=majority';
+
+
+// Connect to MongoDB
+mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+// Check the connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+console.log('Connected to MongoDB!');
+// You can add additional code here for testing or other operations
+// Make sure to close the connection when you're done
+mongoose.connection.close();
+});
+
+
+
 
